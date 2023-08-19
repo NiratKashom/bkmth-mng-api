aa2c2e9da6914110651f55a6234df1eeaaefed90
NiratKashom <earthenly@gmail.com>
2023-08-19 22:05:35 +0700

    getDailyReport addition select unit from DB

--- src/controller/report.controller.ts
+++ src/controller/report.controller.ts
@@ -12,15 +12,15 @@ export const getDailyReport = async (req: Request, res: Response) => {
   try {
     const queries = [
       supabase.from('storefront').select('id, title, category,remark, qty, total_price, is_leftover, unit, leftover_amount, leftover_total_price').eq('date', convertedDate),
-      supabase.from('expense').select('id, title, category,remark, qty, total_price').eq('date', convertedDate),
+      supabase.from('expense').select('id, title,unit, category,remark, qty, total_price').eq('date', convertedDate),
     ];
 
-    // Execute all queries concurrently using Promise.all()
     const [storefrontData, expenseData] = await Promise.all(queries);
     const storefrontList: StorefrontItem[] = convertSnakeCaseToCamelCase(storefrontData.data || []);
     const expenseList: ExpenseItem[] = convertSnakeCaseToCamelCase(expenseData.data || []);
     const summaryIncomeData = summarizeIncomeData(storefrontList);
     const summaryExpense = summarizeExpenseData(expenseList);
+
     res.json({
       message: "GET DATA SUCCESSFULLY",
       data: {
@@ -28,6 +28,7 @@ export const getDailyReport = async (req: Request, res: Response) => {
         expense: summaryExpense
       }
     });
+
   } catch (error: any) {
     res.status(500).json({
       statusText: error.statusText,
